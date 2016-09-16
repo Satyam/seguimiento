d1dc5c1f9656a3fe5a19a5e92d1a568b4bcb49c8
Satyam <satyam@satyam.com.ar>
2016-09-16 19:28:43 +0200

    trying with tile caching

--- www/js/index.js
+++ www/js/index.js
@@ -47,16 +47,19 @@ var app = {
 
       var map = null;
       var marker = null;
+      var tiles = null;
       var i = 0;
       var latEl = $('#lat');
       var lngEl = $('#lng');
       var bipEl = $('#bip');
-      var msgEl = $('#msg')
+      var msgEl = $('#msg');
+      var onlineEl = $('#online');
       function onSuccess(position) {
         try {
           bipEl.html('-/|\\'[(i++) % 4]);
           latEl.html(position.coords.latitude);
           lngEl.html(position.coords.longitude);
+          onlineEl
 
           if (map) {
             marker.setLatLng([position.coords.latitude, position.coords.longitude]);
@@ -65,15 +68,21 @@ var app = {
           		center: [position.coords.latitude, position.coords.longitude],
           		zoom: 15
           	});
-            map.addLayer(L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            tiles = L.tileLayerCordova('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-          			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-          	}));
+          			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+              folder: 'LTileLayerCordovaExample',
+              name:   'example',
+              debug: true
+            });
+            map.addLayer(tiles);
+            onlineEl.prop('checked', tiles.isOnline());
             marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
             map.on('click', function (ev) {
               msgEl.html(msgEl.html() + '<pre>' + JSON.stringify(ev.latlng, null, 2) + '</pre>');
             })
           }
+          window.setTimeout(getPosition, 10000);
         }
         catch(err) {
           onError(err);
@@ -87,13 +96,20 @@ var app = {
         );
       }
 
+      onlineEl.on('click', function (ev) {
+          if (onlineEl.prop('checked')) {
+            tiles.goOnline();
+          } else {
+            tiles.goOffline();
+          }
+      });
+
       function getPosition() {
         navigator.geolocation.getCurrentPosition(onSuccess, onError, {
           timeout: 30000
         });
       }
       getPosition();
-      window.setInterval(getPosition, 10000);
     });
   }
 };
