19e44d374162e7d4e6f22ce1315d4dad2dca8f45
Satyam <satyam@satyam.com.ar>
2016-09-17 19:35:14 +0200

    Caching tile files internally

--- www/js/index.js
+++ www/js/index.js
@@ -33,18 +33,18 @@ var app = {
   // The scope of 'this' is the event. In order to call the 'receivedEvent'
   // function, we must explicitly call 'app.receivedEvent(...);'
   onDeviceReady: function() {
-    $(function () {
+    $(function() {
 
-    	$('#map').css({
-    		'height': ($(window.document).height()) + 'px'
-    	});
-    	$(window).resize(function () {
-    		$('#map').css({
-    			'height': ($(window.document).height()) + 'px'
-    		});
-    	});
+      $('#map').css({
+        'height': ($(window.document).height()) + 'px'
+      });
+      $(window).resize(function() {
+        $('#map').css({
+          'height': ($(window.document).height()) + 'px'
+        });
+      });
 
-
+      var ZOOM = 15;
       var map = null;
       var marker = null;
       var tiles = null;
@@ -54,41 +54,78 @@ var app = {
       var bipEl = $('#bip');
       var msgEl = $('#msg');
       var onlineEl = $('#online');
+
       function onSuccess(position) {
         try {
-          bipEl.html('-/|\\'[(i++) % 4]);
-          latEl.html(position.coords.latitude);
-          lngEl.html(position.coords.longitude);
+          var lat = position.coords.latitude, lng = position.coords.longitude;
+          bipEl.html('-/|\\' [(i++) % 4]);
+          latEl.html(lat);
+          lngEl.html(lng);
           onlineEl
 
           if (map) {
-            marker.setLatLng([position.coords.latitude, position.coords.longitude]);
+            marker.setLatLng([lat, lng]);
           } else {
             map = L.map('map', {
-          		center: [position.coords.latitude, position.coords.longitude],
-          		zoom: 15
-          	});
+              center: [lat, lng],
+              zoom: ZOOM
+            });
             tiles = L.tileLayerCordova('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-          			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
               folder: 'LTileLayerCordovaExample',
-              name:   'example',
+              name: 'example',
               debug: true
+            }, function() {
+              //----------------------------------------------------------------
+              // From: https://github.com/gregallensworth/L.TileLayer.Cordova
+              // calculate a tile pyramid starting at a lat/lon and going down to a stated range of zoom levels
+              var tile_list = tiles.calculateXYZListFromBounds(map.getBounds(), ZOOM - 1, ZOOM + 1);
+              tiles.downloadXYZList(
+                // 1st param: a list of XYZ objects indicating tiles to download
+                tile_list,
+                // 2nd param: overwrite existing tiles on disk?
+                // if no then a tile already on disk will be kept, which can be a big time saver
+                true,
+                // 3rd param: progress callback
+                // receives the number of tiles downloaded and the number of tiles total
+                // caller can calculate a percentage, update progress bar, etc.
+                function(done, total) {
+                  // var percent = Math.round(100 * done / total);
+                  // console.log(done + " / " + total + " = " + percent + "%");
+                },
+                // 4th param: complete callback
+                // no parameters are given, but we know we're done!
+                function() {
+                  // for this demo, on success we use another L.TileLayer.Cordova feature and show the disk usage!
+                  tiles.getDiskUsage(function(filecount, bytes) {
+                    var kilobytes = Math.round(bytes / 1024);
+                    msgEl.html(msgEl.html() +
+                      "Done" + "<br/>" + filecount + " files" + "<br/>" + kilobytes + " kB"
+                    );
+                  });
+                },
+                // 5th param: error callback
+                // parameter is the error message string
+                function(error) {
+                  alert("Failed\nError code: " + error.code);
+                }
+              );
             });
             map.addLayer(tiles);
             onlineEl.prop('checked', tiles.isOnline());
-            marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
-            map.on('click', function (ev) {
+            marker = L.marker([lat, lng]).addTo(map);
+            map.on('click', function(ev) {
               msgEl.html(msgEl.html() + '<pre>' + JSON.stringify(ev.latlng, null, 2) + '</pre>');
-            })
+            });
           }
           window.setTimeout(getPosition, 10000);
-        }
-        catch(err) {
+        } catch (err) {
           onError(err);
         }
 
       };
+
       function onError(error) {
         msgEl.html(msgEl.html() + '<pre>' +
           'code: ' + error.code + '\n' +
@@ -96,12 +133,12 @@ var app = {
         );
       }
 
-      onlineEl.on('click', function (ev) {
-          if (onlineEl.prop('checked')) {
-            tiles.goOnline();
-          } else {
-            tiles.goOffline();
-          }
+      onlineEl.on('click', function(ev) {
+        if (onlineEl.prop('checked')) {
+          tiles.goOnline();
+        } else {
+          tiles.goOffline();
+        }
       });
 
       function getPosition() {
