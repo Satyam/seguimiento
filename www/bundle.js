f8eec8faf696f26e9ac881cdf300cd0fb66de308
Satyam <satyam@satyam.com.ar>
2016-09-28 20:02:36 +0200

    further split

--- www/bundle.js
+++ www/bundle.js
@@ -46,44 +46,20 @@
 
 	$(function() {
 	  var FS = __webpack_require__(1);
-
+	  var MAP = __webpack_require__(2);
 	  var ZOOM = 16;
-	  var map = null;
-	  var marker = null;
-	  var tiles = null;
 	  var i = 0;
-	  var $map = $('#map');
 	  var $lat = $('#lat');
 	  var $lng = $('#lng');
 	  var $bip = $('#bip');
 	  var $msg = $('#msg');
 	  var $onlineStatus = $('#onlineStatus');
-	  var $forceOffline = $('#forceOffline');
-
-
 
 	  document.addEventListener('deviceready', onDeviceReady, false);
 
 	  function onDeviceReady() {
 
-	    $map.css({
-	      'height': ($(window.document).height()) + 'px'
-	    });
-	    $(window).resize(function() {
-	      $map.css({
-	        'height': ($(window.document).height()) + 'px'
-	      });
-	    });
-	    $forceOffline.on('click', function() {
-	      if ($forceOffline.hasClass('btn-warning')) {
-	        tiles.goOnline();
-	        $forceOffline.html('force offline');
-	      } else {
-	        tiles.goOffline();
-	        $forceOffline.html('offline (forced)');
-	      }
-	      $forceOffline.toggleClass('btn-warning');
-	    });
+	    MAP.init();
 
 	    $('#clearCache').click(function () {
 	      FS.getDiskUsage().then(function(result) {
@@ -96,14 +72,10 @@
 	    });
 
 	    document.addEventListener("offline", function() {
-	      tiles.goOffline();
 	      onlineStatus();
 	    }, false);
 
 	    document.addEventListener("online", function() {
-	      if (!$forceOffline.hasclass('btn-warning')) {
-	        tiles.goOnline();
-	      }
 	      onlineStatus();
 	    }, false);
 
@@ -131,47 +103,6 @@
 	  }
 
 
-	  function createMap(lat, lng, zoom) {
-	    map = L.map('map', {
-	      center: [lat, lng],
-	      zoom: zoom
-	    });
-	    map.zoomControl.setPosition('topright');
-	    FS.openFs('seguimiento')
-	    .then(function () {
-	      tiles = new L.TileLayer.Functional(function (view) {
-	        var coords = {
-	          x: view.tile.column,
-	          y: view.tile.row,
-	          z: view.zoom
-	        };
-	        return FS.fileExists(coords)
-	        .then(function (exists) {
-	          if (exists) {
-	            return FS.internalURL(coords);
-	          }
-	          return FS.download(coords)
-	          .then(function () {
-	            return FS.internalURL(coords);
-	          });
-	        });
-	      }, {
-	        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-	          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-	      });
-	      map.addLayer(tiles);
-	    }).catch(onError);
-
-	    map.on('moveend', function (ev) {
-	      console.log('moveend', ev);
-
-	    });
-	    map.on('zoomend', function (ev) {
-	      console.log('zoomend', ev);
-	    });
-	  }
-
-
 	  function newPosition(position) {
 	    var lat = position.coords.latitude,
 	      lng = position.coords.longitude;
@@ -179,15 +110,7 @@
 	    $lat.html(Math.round(lat * 1000) / 1000);
 	    $lng.html(Math.round(lng * 1000) / 1000);
 
-	    if (map) {
-	      marker.setLatLng([lat, lng]);
-	    } else {
-	      createMap(lat, lng, ZOOM);
-	      marker = L.marker([lat, lng]).addTo(map);
-	      map.on('click', function(ev) {
-	        appendMsg(JSON.stringify(ev.latlng, null, 2));
-	      });
-	    }
+	    MAP.position(lat, lng, ZOOM).catch(onError);
 	  }
 
 	  function appendMsg(text) {
@@ -353,6 +276,82 @@
 	};
 
 	module.exports = FS;
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var $map = null;
+	var map = null;
+	var marker = null;
+	var tiles = null;
+	var FS = __webpack_require__(1);
+
+	var MAP = {
+	  init: function () {
+	    $map = $('#map');
+	    $map.css({
+	      'height': ($(window.document).height()) + 'px'
+	    });
+	    $(window).resize(function() {
+	      $map.css({
+	        'height': ($(window.document).height()) + 'px'
+	      });
+	    });
+	  },
+	  getMap: function () {
+	    return map;
+	  },
+	  get$Map: function () {
+	    return $map;
+	  },
+	  createMap: function (lat, lng, zoom) {
+	    map = L.map('map', {
+	      center: [lat, lng],
+	      zoom: zoom
+	    });
+	    map.zoomControl.setPosition('topright');
+	    marker = L.marker([lat, lng]).addTo(map);
+	    return FS.openFs('seguimiento')
+	    .then(function () {
+	      tiles = new L.TileLayer.Functional(function (view) {
+	        var coords = {
+	          x: view.tile.column,
+	          y: view.tile.row,
+	          z: view.zoom
+	        };
+	        return FS.fileExists(coords)
+	        .then(function (exists) {
+	          if (exists) {
+	            return FS.internalURL(coords);
+	          }
+	          return FS.download(coords)
+	          .then(function () {
+	            return FS.internalURL(coords);
+	          });
+	        });
+	      }, {
+	        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+	          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	      });
+	      map.addLayer(tiles);
+	    });
+	  },
+	  position: function (lat, lng, ZOOM) {
+
+	    if (map) {
+	      marker.setLatLng([lat, lng]);
+	      return Promise.resolve();
+	    } else {
+	      return MAP.createMap(lat, lng, ZOOM);
+	    }
+	  }
+
+
+	};
+
+	module.exports = MAP;
 
 
 /***/ }
---
+++ www/js/map.js
@@ -0,0 +1,70 @@
+var $map = null;
+var map = null;
+var marker = null;
+var tiles = null;
+var FS = require('./fs.js');
+
+var MAP = {
+  init: function () {
+    $map = $('#map');
+    $map.css({
+      'height': ($(window.document).height()) + 'px'
+    });
+    $(window).resize(function() {
+      $map.css({
+        'height': ($(window.document).height()) + 'px'
+      });
+    });
+  },
+  getMap: function () {
+    return map;
+  },
+  get$Map: function () {
+    return $map;
+  },
+  createMap: function (lat, lng, zoom) {
+    map = L.map('map', {
+      center: [lat, lng],
+      zoom: zoom
+    });
+    map.zoomControl.setPosition('topright');
+    marker = L.marker([lat, lng]).addTo(map);
+    return FS.openFs('seguimiento')
+    .then(function () {
+      tiles = new L.TileLayer.Functional(function (view) {
+        var coords = {
+          x: view.tile.column,
+          y: view.tile.row,
+          z: view.zoom
+        };
+        return FS.fileExists(coords)
+        .then(function (exists) {
+          if (exists) {
+            return FS.internalURL(coords);
+          }
+          return FS.download(coords)
+          .then(function () {
+            return FS.internalURL(coords);
+          });
+        });
+      }, {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      });
+      map.addLayer(tiles);
+    });
+  },
+  position: function (lat, lng, ZOOM) {
+
+    if (map) {
+      marker.setLatLng([lat, lng]);
+      return Promise.resolve();
+    } else {
+      return MAP.createMap(lat, lng, ZOOM);
+    }
+  }
+
+
+};
+
+module.exports = MAP;
