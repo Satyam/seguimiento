e9a6cfda311b708234dc4d4c0b3b8a628bd0bd5c
Satyam <satyam@satyam.com.ar>
2016-10-05 16:07:58 +0200

    updated bundle

--- www/bundle.js
+++ www/bundle.js
@@ -60,6 +60,22 @@
 	  function onDeviceReady() {
 	    MAP.init();
 
+	    window.sqlitePlugin.echoTest(
+	      function() {
+	        appendMsg('ECHO test OK');
+	      },
+	      function (error) {
+	        appendMsg('Echo test ' + error);
+	      }
+	    );
+	    window.sqlitePlugin.selfTest(
+	      function() {
+	        appendMsg('SELF test OK');
+	      },
+	      function (error) {
+	        appendMsg('Self test ' + error);
+	      }
+	    );
 	    $('#clearCache').click(function () {
 	      FS.getDiskUsage().then(function(result) {
 	        appendMsg(JSON.stringify(result, null, 2));
